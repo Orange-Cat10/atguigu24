06fe8dc25d843d32aae5e13a307a20f25ff49b49
YS-desktop <yangoo6910@gmail.com>
2024-12-03 16:49:55 +0800

    k chapter 9

---
+++ 课件及代码/第9章 模块/代码/code.py
@@ -0,0 +1,15 @@
+# @Author    :ZhanYangshuo
+# @FileName  :code.py
+# @CreateTime:2024/12/2 21:23 
+# @Project   :atguigu24
+# @Software  :PyCharm
+# @Version   :1.0
+
+def sum(a,b) -> int:
+    """
+    求和两个
+    :param a: 第一个
+    :param b: 第二个
+    :return: 反
+    """
+    return a+b
---
+++ 课件及代码/第9章 模块/代码/my.py
@@ -0,0 +1,9 @@
+# @Author    :ZhanYangshuo
+# @FileName  :my.py
+# @CreateTime:2024/12/2 21:25 
+# @Project   :atguigu24
+# @Software  :PyCharm
+# @Version   :1.0
+
+import code
+print(code.sum(1,2))
