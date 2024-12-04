a577d4d156db3e2ee74b16bad9b4238e662fd98b
YS-desktop <yangoo6910@gmail.com>
2024-12-04 17:53:03 +0800

    k chapter 11

---
+++ 课件及代码/第11章 面向对象程序设计/代码/t10.py
@@ -0,0 +1,11 @@
+# @Author    :ZhanYangshuo
+# @FileName  :t10.py
+# @CreateTime:2024/12/4 16:32 
+# @Project   :atguigu24
+# @Software  :PyCharm
+# @Version   :1.0
+
+print('是否是辅导员:%s' % ['否', '是'][True])
+print('是否是辅导员:%s' % ['否', '是'][False])
+print(f'是否是辅导员:{['否','是'][True]}' )
+print(f'是否是辅导员:{['否','是'][False]}' )
